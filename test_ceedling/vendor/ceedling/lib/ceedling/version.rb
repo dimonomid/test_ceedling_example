6adfe65856f74698dddc58e7d66264048fa37518
Dmitry Frank <mail@dmitryfrank.com>
2015-10-03 02:16:00 +0300

    added test_ceedling

---
+++ test_ceedling/vendor/ceedling/lib/ceedling/version.rb
@@ -0,0 +1,15 @@
+# @private
+module Ceedling
+  module Version
+    # @private
+    GEM = "0.18.0"
+    # @private
+    CEEDLING = GEM
+    # @private
+    CEXCEPTION = "1.3.17"
+    # @private
+    CMOCK = "2.3.0"
+    # @private
+    UNITY = "2.2.0"
+  end
+end
